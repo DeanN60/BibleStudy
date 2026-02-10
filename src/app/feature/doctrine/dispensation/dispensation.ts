a933e2a08c0750b187a2c283a6c9d2435e6e66d6
DeanN60 <dneighbour54@gmail.com>
2026-02-10 14:43:18 -0500

    add verses

--- src/app/feature/doctrine/dispensation/dispensation.ts
+++ src/app/feature/doctrine/dispensation/dispensation.ts
@@ -1,16 +1,38 @@
 import { Component } from '@angular/core';
 import {SubMenu} from "../doctrine-sub-menu/sub-menu";
 import {RightSubMenu} from './right-sub-menu/right-sub-menu';
+import {VerseData} from '../../../core/verse/verse-data';
+import {Verse} from '../../../core/verse/verse';
 
 @Component({
   selector: 'app-dispensation',
   imports: [
     SubMenu,
-    RightSubMenu
+    RightSubMenu,
+    Verse
   ],
   templateUrl: './dispensation.html',
   styleUrl: './dispensation.scss',
 })
 export class Dispensation {
-
+  iCor_9_17 = new VerseData(
+    "I Corinthians 9:17",
+    "17 For if I do this thing willingly, I have a reward: but if against my will, a dispensation of the gospel is committed unto me.",
+    "King James Version"
+  );
+  eph_1_10 = new VerseData(
+    "Ephesians 1:10",
+    "10 That in the dispensation of the fulness of times he might gather together in one all things in Christ, both which are in heaven, and which are on earth; even in him:",
+    "King James Version"
+  );
+  eph_3_2 = new VerseData(
+    "Ephesians 3:2",
+    "2 If ye have heard of the dispensation of the grace of God which is given me to you-ward:",
+    "King James Version"
+  );
+  col_1_25 = new VerseData(
+    "Colossians 1:25",
+    "25 Whereof I am made a minister, according to the dispensation of God which is given to me for you, to fulfil the word of God;",
+    "King James Version"
+  )
 }
--- src/app/feature/doctrine/salvation/salvation.ts
+++ src/app/feature/doctrine/salvation/salvation.ts
@@ -15,4 +15,14 @@ import {VerseData} from '../../../core/verse/verse-data';
   styleUrl: './salvation.scss',
 })
 export class Salvation {
+  rom_5_15 = new VerseData(
+    "Romans 5:15",
+    "15 But not as the offence, so also is the free gift. For if through the offence of one many be dead, much more the grace of God, and the gift by grace, which is by one man, Jesus Christ, hath abounded unto many.",
+    "King James Version"
+  );
+  rom_11_6 = new VerseData(
+    "Romans 11:6",
+    "6 And if by grace, then is it no more of works: otherwise grace is no more grace. But if it be of works, then is it no more grace: otherwise work is no more work.",
+    "King James Version"
+  );
 }
