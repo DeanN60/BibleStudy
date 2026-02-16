bae0a4ad6c54d1df4e9957d0f46d55e3e16d29af
DeanN60 <dneighbour54@gmail.com>
2026-02-16 12:46:44 -0500

    fix failing test

--- src/app/feature/doctrine/salvation/eternal-salvation/eternal-salvation.spec.ts
+++ src/app/feature/doctrine/salvation/eternal-salvation/eternal-salvation.spec.ts
@@ -1,6 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { EternalSalvation } from './eternal-salvation';
+import {EternalSalvation} from './eternal-salvation';
+import {provideRouter} from '@angular/router';
+import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClientTesting} from '@angular/common/http/testing';
 
 describe('EternalSalvation', () => {
   let component: EternalSalvation;
@@ -8,9 +11,15 @@ describe('EternalSalvation', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EternalSalvation]
-    })
-    .compileComponents();
+      imports: [
+        EternalSalvation
+      ],
+      providers: [
+        provideRouter([]),
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(EternalSalvation);
     component = fixture.componentInstance;
