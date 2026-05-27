b5f7b3676103c38bc8dc7e1e57dd23995554d9c0
DeanN60 <dneighbour54@gmail.com>
2026-05-27 08:36:21 -0400

    add themes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {Footer} from '@core/footer';
 import {Header} from '@core/header';
 import {Menu} from '@core/menu';
+import {MyThemePreset} from './theme/theme';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,14 @@ import {Menu} from '@core/menu';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
+
+  ngOnInit(): void {
+    console.log('MyThemePreset:color', MyThemePreset.primitive)
+    console.log('MyThemePreset:css', MyThemePreset.css)
+    console.log('MyThemePreset:Semantic ', MyThemePreset.semantic);
+    console.log('MyThemePreset:accordion', MyThemePreset.components?.accordion);
+  }
+
   protected readonly title = signal('BibleStudy');
 }
