2470f7b7203ae9743620b925d35a6b5ec5108d3a
DeanN60 <dneighbour54@gmail.com>
2026-05-27 08:35:19 -0400

    add primitive.ts add semantic.ts

---
+++ src/app/theme/primitive.ts
@@ -0,0 +1,7 @@
+import {DarkTeal} from './color/dark-teal';
+
+export const Primitive = {
+  primitive: {
+   darkTeal: DarkTeal
+  }
+}
---
+++ src/app/theme/semantic.ts
@@ -0,0 +1,14 @@
+import {DarkTealSemantic} from './color/dark-teal';
+
+export const Semantic = {
+  semantic: {
+    content: {
+      borderRadius: '{border.radius.ls}',
+    },
+    colorScheme: {
+      dark: {
+        surface: DarkTealSemantic
+      }
+    }
+  }
+};
