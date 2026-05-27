3d3511ff3bd1fc52cf9a31e4106ae582f70a5041
DeanN60 <dneighbour54@gmail.com>
2026-05-27 08:33:36 -0400

    add matt_7.ts add matt_25.ts

---
+++ src/app/core/verse/books/Matthew/matt_25.ts
@@ -0,0 +1,85 @@
+import {VerseData} from '@core/verse-data';
+
+export const matt_25_1_13 = new VerseData(
+  "Matthew 25:1-13",
+  "1 “Then the kingdom of heaven may be compared to ten virgins, who took their lamps and went out to meet the " +
+  "bridegroom. 2 Now five of them were foolish, and five were prudent. 3 For when the foolish took their lamps, " +
+  "they took no oil with them, 4 but the prudent took oil in flasks along with their lamps. 5 Now while the " +
+  "bridegroom was delaying, they all got drowsy and began to sleep. 6 But at midnight there was a shout, ‘Behold, " +
+  "the bridegroom! Come out to meet him.’ 7 Then all those virgins rose and trimmed their lamps. 8 And the foolish " +
+  "said to the prudent, ‘Give us some of your oil, for our lamps are going out.’ 9 But the prudent answered, saying, " +
+  "‘No, there will not be enough for us and you too; go instead to the dealers and buy some for yourselves.’ " +
+  "10 And while they were going away to make the purchase, the bridegroom came, and those who were ready went in " +
+  "with him to the wedding feast; and the door was shut. 11 And later the other virgins also came, saying, " +
+  "‘Lord, lord, open up for us.’ 12 But he answered and said, ‘Truly I say to you, I do not know you.’ " +
+  "13 Therefore, stay awake, for you do not know the day nor the hour.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3323). 316 Publishing, Inc.. Kindle Edition. "
+);
+
+export const matt_25_14_29 = new VerseData(
+  "Matthew 25:14-29",
+  "14 “For it is just like a man about to go on a journey, who called his own slaves and handed over his" +
+  " possessions to them. 15 And to one he gave five talents, to another, two, and to another, one, each " +
+  "according to his own ability; and he went on his journey. 16 Immediately the one who had received the " +
+  "five talents went and traded with them, and gained five more talents. 17 In the same manner the one who " +
+  "had received the two talents gained two more. 18 But he who received the one talent went away, and dug a " +
+  "hole in the ground and hid his master’s money. 19 “Now after a long time the master of those slaves came " +
+  "and settled accounts with them. 20 And the one who had received the five talents came up and brought five " +
+  "more talents, saying, ‘Master, you handed five talents over to me. See, I have gained five more talents.’ " +
+  "21 His master said to him, ‘Well done, good and faithful slave. You were faithful with a few things, I will " +
+  "put you in charge of many things; enter into the joy of your master.’ 22 “Also the one who had received the " +
+  "two talents came up and said, ‘Master, you handed two talents over to me. See, I have gained two more talents.’ " +
+  "23 His master said to him, ‘Well done, good and faithful slave. You were faithful with a few things, I will put " +
+  "you in charge of many things; enter into the joy of your master.’ 24 “And the one also who had received the one " +
+  "talent came up and said, ‘Master, I knew you to be a hard man, reaping where you did not sow and gathering where " +
+  "you scattered no seed. 25 And I was afraid, and went away and hid your talent in the ground. See, you have what is " +
+  "yours.’ 26 “But his master answered and said to him, ‘You wicked, lazy slave, you knew that I reap where I did not " +
+  "sow and gather where I scattered no seed. 27 Therefore, you ought to have put my money ain the bank, and on my " +
+  "arrival I would have received my money back with interest. 28 Therefore take away the talent from him, and give it " +
+  "to the one who has the ten talents.’ 29 “For to everyone who has, more shall be given, and he will have an abundance; " +
+  "but from the one who does not have, even what he does have shall be taken away. 30 And throw out the worthless slave " +
+  "into the outer darkness; in that place there will be weeping and gnashing of teeth.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (pp. 3323-3325). 316 Publishing, Inc.. Kindle Edition."
+);
+
+export const matt_25_19_21 = new VerseData(
+  "Matthew 25:19-21",
+  "19 “Now after a long time the master of those slaves came and settled accounts with them. 20 And the one who " +
+  "had received the five talents came up and brought five more talents, saying, ‘Master, you handed five talents " +
+  "over to me. See, I have gained five more talents.’ 21 His master said to him, ‘Well done, good and faithful " +
+  "slave. You were faithful with a few things, I will put you in charge of many things; enter into the joy of your master.’",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3324). 316 Publishing, Inc.. Kindle Edition. "
+);
+
+export const matt_25_22_23 = new VerseData(
+  "Matthew 25:22-23",
+  "22 “Also the one who had received the two talents came up and said, ‘Master, you handed two talents over to me. " +
+  "See, I have gained two more talents.’ 23 His master said to him, ‘Well done, good and faithful slave. You were " +
+  "faithful with a few things, I will put you in charge of many things; enter into the joy of your master.’",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3324). 316 Publishing, Inc.. Kindle Edition. "
+);
+
+export const matt_25_24_25 = new VerseData(
+  "Matthew 25:24-25",
+  "24 “And the one also who had received the one talent came up and said, ‘Master, I knew you to be a hard man, " +
+  "reaping where you did not sow and gathering where you scattered no seed. 25 And I was afraid, and went away and " +
+  "hid your talent in the ground. See, you have what is yours.’",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3324). 316 Publishing, Inc.. Kindle Edition. "
+);
+
+export const matt_25_26_28 = new VerseData(
+  "Matthew 25:26-28",
+  "26 “But his master answered and said to him, ‘You wicked, lazy slave, you knew that I reap where I did not " +
+  "sow and gather where I scattered no seed. 27 Therefore, you ought to have put my money ain the bank, and on my " +
+  "arrival I would have received my money back with interest. 28 Therefore take away the talent from him, and give " +
+  "it to the one who has the ten talents.’",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3324). 316 Publishing, Inc.. Kindle Edition."
+);
+
+export const matt_25_29_30 = new VerseData(
+  "Matthew 25:29-30",
+  "29 “For to everyone who has, more shall be given, and he will have an abundance; but from the one who does not " +
+  "have, even what he does have shall be taken away. 30 And throw out the worthless slave into the outer darkness; " +
+  "in that place there will be weeping and gnashing of teeth.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (pp. 3324-3325). 316 Publishing, Inc.. Kindle Edition. "
+);
---
+++ src/app/core/verse/books/Matthew/matt_7.ts
@@ -0,0 +1,9 @@
+import {VerseData} from '@core/verse-data';
+
+export const matt_7_21_23 = new VerseData(
+  "Matthew 7:21-23",
+  "21 Not every one that saith unto me, Lord, Lord, shall enter into the kingdom of heaven; but he that doeth the will of my Father which is in heaven." +
+  "22 Many will say to me in that day, Lord, Lord, have we not prophesied in thy name? and in thy name have cast out devils? and in thy name done many wonderful works?" +
+  "23 And then will I profess unto them, I never knew you: depart from me, ye that work iniquity.",
+  "King James Version"
+);
