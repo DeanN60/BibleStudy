1f413c6f3071bb8dd71400fb46d4ef071780faba
DeanN60 <dneighbour54@gmail.com>
2026-05-30 08:46:28 -0400

    add john_1_14

---
+++ src/app/core/verse/books/John/john_1_14.ts
@@ -0,0 +1,7 @@
+import {VerseData} from '@core/verse-data';
+
+export const john_1_14 = new VerseData(
+  "John 1:14",
+  "14 And the Word became flesh, and dwelt among us, and we beheld His glory, glory as of the only begotten from the Father, full of grace and truth.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3561). 316 Publishing, Inc.. Kindle Edition."
+);
