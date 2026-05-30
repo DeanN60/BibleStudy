e6f5a6894677b837a2d990a398c0e8f02b9ec4a1
DeanN60 <dneighbour54@gmail.com>
2026-05-30 08:47:50 -0400

    add text and verse

--- src/app/feature/jesus/incarnate/incarnate.ts
+++ src/app/feature/jesus/incarnate/incarnate.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import {JesusSubMenu} from '../jesus-sub-menu/jesus-sub-menu';
+import {Verse} from '@core/verse';
+import {john_1_14} from '@core/books/John/john_1_14';
 
 @Component({
   selector: 'app-incarnate',
   imports: [
-    JesusSubMenu
+    JesusSubMenu,
+    Verse
   ],
   templateUrl: './incarnate.html',
   styleUrl: '../jesus.scss',
 })
 export class Incarnate {
 
+  protected readonly john_1_14 = john_1_14;
 }
