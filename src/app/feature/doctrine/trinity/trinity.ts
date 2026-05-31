ce42c02ac4d143ee4666b96e38b467f0423408d4
DeanN60 <dneighbour54@gmail.com>
2026-05-31 07:19:36 -0400

    update trinity

--- src/app/feature/doctrine/trinity/trinity.ts
+++ src/app/feature/doctrine/trinity/trinity.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import {SubMenu} from "../doctrine-sub-menu/sub-menu";
+import {Verse} from '@core/verse';
+import {john_1_1_5} from '@core/books/John/john_1_1_5';
 
 @Component({
   selector: 'app-trinity',
-    imports: [
-        SubMenu
-    ],
+  imports: [
+    SubMenu,
+    Verse
+  ],
   templateUrl: './trinity.html',
   styleUrl: './trinity.scss',
 })
 export class Trinity {
 
+  protected readonly john_1_1_5 = john_1_1_5;
 }
