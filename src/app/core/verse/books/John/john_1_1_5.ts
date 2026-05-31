7675c7dbcfc706eaad6c21c953534656f4f2bc99
DeanN60 <dneighbour54@gmail.com>
2026-05-31 07:19:55 -0400

    add john_1_1_5

---
+++ src/app/core/verse/books/John/john_1_1_5.ts
@@ -0,0 +1,7 @@
+import {VerseData} from '@core/verse-data';
+
+export const john_1_1_5 = new VerseData(
+  "John 1:1-5",
+  "1 In the beginning was the Word, and the Word was with God, and the Word was God. 2 He was in the beginning with God. 3 All things came into being through Him, and apart from Him nothing came into being that has come into being. 4 In Him was life, and the life was the Light of men. 5 And the Light shines in the darkness, and the darkness did not overtake it.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3561). 316 Publishing, Inc.. Kindle Edition."
+)
