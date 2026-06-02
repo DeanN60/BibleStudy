b12e19d9abb59a1a8172ec22020307daeea17263
DeanN60 <dneighbour54@gmail.com>
2026-06-02 10:42:21 -0400

    create gen_6.ts

---
+++ src/app/core/verse/books/Genesis/gen_6.ts
@@ -0,0 +1,7 @@
+import {VerseData} from '@core/verse-data';
+
+export const gen_6_5 = new VerseData(
+  "Genesis 6:5",
+"5 Then Yahweh saw that the evil of man was great on the earth, and that every intent of the thoughts of his heart was only evil continually.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 107). 316 Publishing, Inc.. Kindle Edition."
+);
