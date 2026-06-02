d8076a249840c6737cde0eb6baa2bc5d1c8d2fb2
DeanN60 <dneighbour54@gmail.com>
2026-06-02 10:41:06 -0400

    add rom_8_7_8

--- src/app/core/verse/books/Romans/rom_8.ts
+++ src/app/core/verse/books/Romans/rom_8.ts
@@ -6,6 +6,12 @@ export const rom_8_1 = new VerseData(
   "King James Version"
 );
 
+export const rom_8_7_8 = new VerseData(
+  "Romans 8:7-8",
+  "7 because the mind set on the flesh is hostile toward God, for it does not subject itself to the law of God, for it is not even able to do so, 8 and those who are in the flesh are not able to please God.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3782). 316 Publishing, Inc.. Kindle Edition."
+)
+
 export const rom_8_38_39 = new VerseData(
   "Romans 8:38,39",
   "38 For I am persuaded, that neither death, nor life, nor angels, nor principalities, nor powers, nor things present, nor things to come," +
