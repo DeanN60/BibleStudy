ebee4a0cdbc45ab23d68f1a54c56f29281c3498b
DeanN60 <dneighbour54@gmail.com>
2026-06-02 08:11:05 -0400

    add isaiah_26_19_21

---
+++ src/app/core/verse/books/Isaiah/isaiah_26_19_21.ts
@@ -0,0 +1,7 @@
+import {VerseData} from '@core/verse-data';
+
+export const Isaiah_26_19_21 = new VerseData(
+  "Isaiah 26:19-21",
+  "19 Your dead will live; Their corpses will rise. You who dwell in the dust, awake and shout for joy, For your dew is as the dew of the dawn, And the earth will give birth to the departed spirits. 20 Come, my people, enter into your rooms And close your doors behind you; Hide for a little awhile Until indignation passes by. 21 For behold, Yahweh is about to come out from His place To visit the iniquity of the inhabitants of the earth; And the earth will reveal her bloodshed And will no longer cover those of hers who were killed.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (pp. 2409-2410). 316 Publishing, Inc.. Kindle Edition. ",
+);
