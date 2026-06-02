67dee20b704b74310c2019ef4b470ef67a183030
DeanN60 <dneighbour54@gmail.com>
2026-06-02 10:40:42 -0400

    renamed gen_5_3_4.ts to gen_5.ts

--- src/app/core/verse/books/Genesis/gen_5.ts
+++ src/app/core/verse/books/Genesis/gen_5.ts
@@ -1,6 +1,6 @@
 import {VerseData} from '@core/verse-data';
 
-export const gen_5_3_4 = new VerseData(
+export const gen_5 = new VerseData(
   "Genesis 5:3,4",
   "3 Adam was 130 years old when Seth was born 4 Adam lived 800 after Seth was born",
   "King James Version"
