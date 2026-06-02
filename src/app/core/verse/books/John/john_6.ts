1cf3dd734aa11c0c90ff941e0ca7231561b0fce6
DeanN60 <dneighbour54@gmail.com>
2026-06-02 10:42:59 -0400

    create john_6.ts

---
+++ src/app/core/verse/books/John/john_6.ts
@@ -0,0 +1,19 @@
+import {VerseData} from '@core/verse-data';
+
+export const john_6_35 = new VerseData(
+  "John 6:35",
+  "35 Jesus said to them, “I am the bread of life. He who comes to Me will never hunger, and he who believes in Me will never thirst.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3580). 316 Publishing, Inc.. Kindle Edition."
+);
+
+export const john_6_37 = new VerseData(
+  "John 6:37",
+  "37 All that the Father gives Me will come to Me, and the one who comes to Me I will never cast out.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3580). 316 Publishing, Inc.. Kindle Edition."
+)
+
+export const john_6_44 = new VerseData(
+  "John 6:44",
+  "44 No one can come to Me unless the Father who sent Me draws him; and I will raise him up on the last day.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (pp. 3580-3581). 316 Publishing, Inc.. Kindle Edition."
+);
