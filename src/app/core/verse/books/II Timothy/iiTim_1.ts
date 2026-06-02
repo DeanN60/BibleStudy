9792adcfcdbafc328577a6ee94cd2318e00167a6
DeanN60 <dneighbour54@gmail.com>
2026-06-02 10:42:33 -0400

    create iiTim_1.ts

---
+++ src/app/core/verse/books/II Timothy/iiTim_1.ts
@@ -0,0 +1,7 @@
+import {VerseData} from '@core/verse-data';
+
+export const iiTim_1_9 = new VerseData(
+  "II Timothy 1:9",
+  "9 who has saved us and called us with a holy calling, not according to our works, but according to His own purpose and grace which was given to us in Christ Jesus from all eternity,",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3993). 316 Publishing, Inc.. Kindle Edition. "
+)
