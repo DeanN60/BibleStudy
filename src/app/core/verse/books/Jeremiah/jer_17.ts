abaf7936a14107b99d7da9831a32ddc8a0d34898
DeanN60 <dneighbour54@gmail.com>
2026-06-02 10:42:46 -0400

    create jer_17.ts

---
+++ src/app/core/verse/books/Jeremiah/jer_17.ts
@@ -0,0 +1,7 @@
+import {VerseData} from '@core/verse-data';
+
+export const jer_17_9 = new VerseData(
+  "Jeremiah 17:9",
+  "9 The heart is more deceitful than all else And is desperately sick; Who can know it?",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 2629). 316 Publishing, Inc.. Kindle Edition."
+);
