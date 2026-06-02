1aa86d84c899ff061c54b79f7a73cf706b24ec56
DeanN60 <dneighbour54@gmail.com>
2026-06-02 08:10:14 -0400

    add life-of-jesus

---
+++ src/app/feature/time-lines/life-of-jesus/life-of-jesus.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LifeOfJesus } from './life-of-jesus';
+
+describe('LifeOfJesus', () => {
+  let component: LifeOfJesus;
+  let fixture: ComponentFixture<LifeOfJesus>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LifeOfJesus]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LifeOfJesus);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
