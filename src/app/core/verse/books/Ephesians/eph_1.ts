04eec83df7ef1caf3d84dc84599f4e85e7d49b54
DeanN60 <dneighbour54@gmail.com>
2026-06-02 10:42:01 -0400

    add eph_1_1_11

--- src/app/core/verse/books/Ephesians/eph_1.ts
+++ src/app/core/verse/books/Ephesians/eph_1.ts
@@ -1,5 +1,11 @@
 import {VerseData} from '@core/verse-data';
 
+export const eph_1_1_11 = new VerseData(
+  "Ephesians 1:1-11",
+  "1 Paul, an apostle of Christ Jesus by the will of God, To the saints who are cat Ephesus and who are faithful in Christ Jesus: 2 Grace to you and peace from God our Father and the Lord Jesus Christ. 3 Blessed be the God and Father of our Lord Jesus Christ, who has blessed us with every spiritual blessing in the heavenly places in Christ, 4 just as He chose us in Him before the foundation of the world, that we would be holy and blameless before Him in love, 5 by predestining us to adoption as sons through Jesus Christ to Himself, according to the good pleasure of His will, 6to the praise of the glory of His grace, which He graciously bestowed on us in the Beloved. 7 In Him we have redemption through His blood, the forgiveness of our transgressions, according to the riches of His grace 8which He caused to abound to us in all wisdom and insight, 9 making known to us the mystery of His will, according to His good pleasure which He purposed in Him 10 for an administration of the fullness of the times, that is, the summing up of all things in Christ, things ain the heavens and things on the earth in Him. 11 In Him, we also have been made an inheritance, having been predestined according to the purpose of Him who works all things according to the counsel of His will,",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3916). 316 Publishing, Inc.. Kindle Edition."
+)
+
 export const eph_1_10 = new VerseData(
   "Ephesians 1:10",
   "10 That in the dispensation of the fulness of times he might gather together in one all things in Christ, both which are in heaven, and which are on earth; even in him:",
