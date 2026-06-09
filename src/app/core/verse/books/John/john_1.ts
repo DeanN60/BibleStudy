467d153dc62ae7c1b80706eb92245933606b759a
DeanN60 <dneighbour54@gmail.com>
2026-06-09 14:59:31 -0400

    reformat verses

--- src/app/core/verse/books/John/john_1.ts
+++ src/app/core/verse/books/John/john_1.ts
@@ -2,7 +2,11 @@ import {VerseData} from '@core/verse-data';
 
 export const john_1 = new VerseData(
   "John 1:1-5 LSB",
-  "1 In the beginning was the Word, and the Word was with God, and the Word was God. 2 He was in the beginning with God. 3 All things came into being through Him, and apart from Him nothing came into being that has come into being. 4 In Him was life, and the life was the Light of men. 5 And the Light shines in the darkness, and the darkness did not overtake it.",
+  "1 In the beginning was the Word, and the Word was with God, and the Word was God. " +
+  "2 He was in the beginning with God. " +
+  "3 All things came into being through Him, and apart from Him nothing came into being that has come into being. " +
+  "4 In Him was life, and the life was the Light of men. " +
+  "5 And the Light shines in the darkness, and the darkness did not overtake it.",
   "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3561). 316 Publishing, Inc.. Kindle Edition."
 )
 
--- src/app/core/verse/books/John/john_10.ts
+++ src/app/core/verse/books/John/john_10.ts
@@ -2,7 +2,12 @@ import {VerseData} from '@core/verse-data';
 
 export const john_10_11_15 = new VerseData(
   "John 10:11-15 LSB",
-  "10 The thief comes only to steal and kill and destroy; I came that they may have life, and have it abundantly. 11 “I am the good shepherd; the good shepherd lays down His life for the sheep. 12 He who is a hired hand, and not a shepherd, who is not the owner of the sheep, sees the wolf coming, and leaves the sheep and flees—and the wolf snatches and scatters them— 13 because he is a hired hand and is not concerned about the sheep. 14 I am the good shepherd, and I know My own and My own know Me, 15 even as the Father knows Me and I know the Father; and I lay down My life for the sheep.",
+  "10 The thief comes only to steal and kill and destroy; I came that they may have life, and have it abundantly. " +
+  "11 “I am the good shepherd; the good shepherd lays down His life for the sheep. " +
+  "12 He who is a hired hand, and not a shepherd, who is not the owner of the sheep, sees the wolf coming, and leaves the sheep and flees—and the wolf snatches and scatters them— " +
+  "13 because he is a hired hand and is not concerned about the sheep. " +
+  "14 I am the good shepherd, and I know My own and My own know Me, " +
+  "15 even as the Father knows Me and I know the Father; and I lay down My life for the sheep.",
   "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3594). 316 Publishing, Inc.. Kindle Edition. "
 );
 
@@ -14,6 +19,8 @@ export const john_10_26 = new VerseData(
 
 export const john_10_27_29 = new VerseData(
   "John 10:27-29 LSB",
-  "27 My sheep hear My voice, and I know them, and they follow Me; 28 and I give eternal life to them, and they will never perish—ever; and no one will snatch them out of My hand. 29 My Father, who has given them to Me, is greater than all; and no one is able to snatch them out of the Father’s hand.",
+  "27 My sheep hear My voice, and I know them, and they follow Me; " +
+  "28 and I give eternal life to them, and they will never perish—ever; and no one will snatch them out of My hand. " +
+  "29 My Father, who has given them to Me, is greater than all; and no one is able to snatch them out of the Father’s hand.",
   "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3595). 316 Publishing, Inc.. Kindle Edition. "
 )
