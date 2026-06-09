c8379f4ba0e4acd4372d160ca7b13bd91936ce93
DeanN60 <dneighbour54@gmail.com>
2026-06-09 14:59:05 -0400

    add john_14

---
+++ src/app/core/verse/books/John/john_14.ts
@@ -0,0 +1,16 @@
+import {VerseData} from '@core/verse-data';
+
+export const john_14_1_4 = new VerseData(
+  "John 14:1-4",
+  "1 “Do not let your heart be troubled; believe in God, believe also in Me. " +
+  "2 In My Father’s house are many dwelling places; if it were not so, I would have told you; for I go to prepare a place for you. " +
+  "3 And if I go and prepare a place for you, I will come again and receive you to Myself, that where I am, there you may be also. " +
+  "4 And you know the way where I am going.”",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3608). 316 Publishing, Inc.. Kindle Edition."
+)
+
+export const john_14_6 = new VerseData(
+  "John 14:6",
+  "6 Jesus said to him, “I am the way, and the truth, and the life. No one comes to the Father but through Me.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3608). 316 Publishing, Inc.. Kindle Edition."
+)
