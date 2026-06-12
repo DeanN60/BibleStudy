5ebc5202d7f49e78347f126aec0e617455a7b3b6
DeanN60 <dneighbour54@gmail.com>
2026-06-12 15:29:50 -0400

    adjust the top

--- src/app/feature/time-lines/life-of-jesus/life-of-jesus.ts
+++ src/app/feature/time-lines/life-of-jesus/life-of-jesus.ts
@@ -20,8 +20,7 @@ export class LifeOfJesus extends BasePageWithSubtitle implements OnInit {
   personList: iPerson[] | undefined;
   markerList: iMarker[] | undefined;
   markerHeight = 50;
-  marginTop = 6;
-  markerTop = -2;
+  marginTop = 5;
   scale = 50;
 
   override init(): void {
@@ -30,10 +29,10 @@ export class LifeOfJesus extends BasePageWithSubtitle implements OnInit {
     // this.personList.push({name: "Resurrection", birthYear: 33, age: 0.15, scale: this.scale});
 
     this.markerList = [];
-    this.markerList.push({name: "12 years", eventYear: 12 * this.scale, top: this.markerTop, height: this.markerHeight, label: {left: -2.1, top: -4.5, labelColor: "white"}});
-    this.markerList.push({name: "Start Ministry", eventYear: 30 * this.scale, top: this.markerTop, height: this.markerHeight, label: {left: -3.5, top: -5.5, labelColor: "white"}});
-    this.markerList.push({name: "Crucifixion", eventYear: 33 * this.scale, top: this.markerTop, height: this.markerHeight, label: {left: -2.8, top: -5, labelColor: "red"}});
-    this.markerList.push({name: "Ascension", eventYear: 33.3 * this.scale, top: this.markerTop, height: this.markerHeight, label: {left: -2.9, top: -5, labelColor: "lightgreen"}});
-    this.markerList.push({name: "Pentecost", eventYear: 33.4 * this.scale, top: this.markerTop, height: this.markerHeight, label: {left: -2.3, top: -5, labelColor: "lightgreen"}});
+    this.markerList.push({name: "12 years", eventYear: 12 * this.scale, height: this.markerHeight, label: {left: -2.1, top: -4.5, labelColor: "white"}});
+    this.markerList.push({name: "Start Ministry", eventYear: 30 * this.scale, height: this.markerHeight, label: {left: -3.5, top: -5.5, labelColor: "white"}});
+    this.markerList.push({name: "Crucifixion", eventYear: 33 * this.scale, height: this.markerHeight, label: {left: -2.8, top: -5, labelColor: "red"}});
+    this.markerList.push({name: "Ascension", eventYear: 33.3 * this.scale, height: this.markerHeight, label: {left: -2.9, top: -5, labelColor: "lightgreen"}});
+    this.markerList.push({name: "Pentecost", eventYear: 33.4 * this.scale, height: this.markerHeight, label: {left: -2.3, top: -5, labelColor: "lightgreen"}});
   }
 }
