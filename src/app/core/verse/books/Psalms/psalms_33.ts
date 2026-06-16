030dff7014c5c4cdb0a6100e2618e5ee87ae067b
DeanN60 <dneighbour54@gmail.com>
2026-06-16 15:10:53 -0400

    add psalms verses

---
+++ src/app/core/verse/books/Psalms/psalms_33.ts
@@ -0,0 +1,7 @@
+import {VerseData} from '@core/verse-data';
+
+export const psalms_33_6 = new VerseData(
+  "Psalms 33:6",
+  "6 By the word of Yahweh the heavens were made, And by the breath of His mouth all their host.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 1908). 316 Publishing, Inc.. Kindle Edition."
+);
