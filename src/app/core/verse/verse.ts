13b12f22c794e1d3a9857f918f5b3cb0f9be690e
DeanN60 <dneighbour54@gmail.com>
2026-06-16 09:05:14 -0400

    add verse-block add app-book selector

--- src/app/core/verse/verse.ts
+++ src/app/core/verse/verse.ts
@@ -1,15 +1,20 @@
 import {Component, input} from '@angular/core';
 import {VerseData} from './verse-data';
+import {VerseBlock} from '@core/verse-block/verse-block';
+import {KeepInViewport} from '@core/directive/keep-in-viewport';
 
-const fallbackVerse: VerseData = {
+export const fallbackVerse: VerseData = {
   reference: "unknown",
   verse: "no verse available",
   source: "unknown"
 }
 
 @Component({
-  selector: 'app-verse',
-  imports: [],
+  selector: 'app-verse, app-book',
+  imports: [
+    VerseBlock,
+    KeepInViewport
+  ],
   templateUrl: './verse.html',
   styleUrl: './verse.scss',
 })
