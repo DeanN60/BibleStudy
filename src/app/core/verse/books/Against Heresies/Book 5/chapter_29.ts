b451270574665bd84024c0d971d02ffabe4df6d3
DeanN60 <dneighbour54@gmail.com>
2026-06-16 09:01:45 -0400

    add reference from Against Heresies

---
+++ src/app/core/verse/books/Against Heresies/Book 5/chapter_29.ts
@@ -0,0 +1,7 @@
+import {VerseData} from '@core/verse-data';
+
+export const againstHereChap29p1 = new VerseData(
+  "Against Heresies Chapter 29 p1",
+  "And therefore, when in the end the Church shall be suddenly caught up from this, it is said, \"There shall be Tribulation such as has not been since the beginning, neither shall be.\" For this is the last contest of the righetous, in which, when they overcome they are crowned with incorruption.",
+  "Against Heresies Book V Chapter 29 page 578 paragraph 1",
+);
