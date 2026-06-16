5fd5fa830a3c4bed94738486445b39e930d9eb3b
DeanN60 <dneighbour54@gmail.com>
2026-06-16 15:09:45 -0400

    add scripture

---
+++ src/app/core/verse/books/Genesis/gen_1.ts
@@ -0,0 +1,10 @@
+import {VerseData} from '@core/verse-data';
+
+export const gen_1_28_31 = new VerseData(
+  "Genesis 1:28-31 LSB",
+  "28 God blessed them, and God said to them, “Be fruitful and multiply, and fill the earth, and subdue it; and have dominion over the fish of the sea and over the birds of the asky and over every living thing that creeps on the earth.” " +
+  "29 Then God said, “Behold, I have given to you every plant yielding seed that is on the asurface of all the earth, and every tree which has the fruit of the tree yielding seed; it shall be food for you; " +
+  "30 and to every beast of the earth and to every bird of the sky and to every thing that creeps on the earth which has life, I have given every green plant for food”; and it was so. " +
+  "31 And God saw all that He had made, and behold, it was very good. And there was evening and there was morning, the sixth day.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (pp. 94-95). 316 Publishing, Inc.. Kindle Edition."
+);
---
+++ src/app/core/verse/books/Genesis/gen_3.ts
@@ -0,0 +1,32 @@
+import {VerseData} from '@core/verse-data';
+
+export const gen_3_1_19 = new VerseData(
+  "Genesis 3:1-19 LSB",
+  "1 Now the serpent was more crafty than any beast of the field which Yahweh God had made. And he said to the woman, “Indeed, has God said, ‘You shall not eat from any tree of the garden’?” " +
+  "2 And the woman said to the serpent, “From the fruit of the trees of the garden we may eat; " +
+  "3 but from the fruit of the tree which is in the midst of the garden, God said, ‘You shall not eat from it, and you shall not touch it, lest you die.’” " +
+  "4 And the serpent said to the woman, “You surely will not die! " +
+  "5 For God knows that in the day you eat from it your eyes will be opened, and you will be like God, knowing good and evil.” " +
+  "6 Then the woman saw that the tree was good for food, and that it was a delight to the eyes, and that the tree was desirable to make one wise, so she took from its fruit and ate; and she gave also to her husband with her, and he ate. " +
+  "7 And the eyes of both of them were opened, and they knew that they were naked; and they sewed fig leaves together and made themselves loin coverings. " +
+  "8 Then they heard the sound of Yahweh God walking in the garden in the acool of the day, and the man and his wife hid themselves from the presence of Yahweh God in the midst of the trees of the garden. " +
+  "9 Yahweh God called to the man and said to him, “Where are you?” " +
+  "10 And he said, “I heard the sound of You in the garden, and I was afraid because I was naked; so I hid.” " +
+  "11 And He said, “Who told you that you were naked? Have you eaten from the tree of which I commanded you not to eat?” " +
+  "12 And the man said, “The woman whom You gave to be with me, she gave to me from the tree, and I ate.” " +
+  "13 Then Yahweh God said to the woman, “What is this you have done?” And the woman said, “The serpent deceived me, and I ate.” " +
+  "14 And Yahweh God said to the serpent, “Because you have done this, Cursed are you more than any of the cattle, And more than every beast of the field; On your belly you will go, And dust you will eat All the days of your life; " +
+  "15 And I will put enmity Between you and the woman, And between your seed and her seed; He shall bruise you on the head, And you shall bruise him on the heel.” " +
+  "16 To the woman He said, “I will greatly multiply Your pain and conception, In pain you will bear children; Your desire will be for your husband, And he will rule over you.” " +
+  "17 Then to Adam He said, “Because you have listened to the voice of your wife and have eaten from the tree about which I commanded you, saying, ‘You shall not eat from it’; Cursed is the ground because of you; In pain you will eat of it All the days of your life. " +
+  "18 Both thorns and thistles it shall grow for you; And you will eat the plants of the field; " +
+  "19 By the sweat of your face You will eat bread, Till you return to the ground, Because from it you were taken; For you are dust, And to dust you shall return.”",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (pp. 98-100). 316 Publishing, Inc.. Kindle Edition."
+);
+
+export const gen_3_23_24 = new VerseData(
+  "Genesis 3:23,24 LSB",
+  "23 therefore Yahweh God sent him out from the garden of Eden, to acultivate the ground from which he was taken. " +
+  "24 So He drove the man out; and at the east of the garden of Eden He stationed the cherubim and the flaming sword which turned every direction to guard the way to the tree of life.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 100). 316 Publishing, Inc.. Kindle Edition. "
+)
