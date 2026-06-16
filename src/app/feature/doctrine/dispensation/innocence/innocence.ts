94f2b5d93f57e38a38d848ecb50200691365520b
DeanN60 <dneighbour54@gmail.com>
2026-06-16 15:12:55 -0400

    add references

--- src/app/feature/doctrine/dispensation/innocence/innocence.ts
+++ src/app/feature/doctrine/dispensation/innocence/innocence.ts
@@ -2,12 +2,17 @@ import {Component} from '@angular/core';
 import {SubMenu} from "../../doctrine-sub-menu/sub-menu";
 import {RightSubMenu} from '../right-sub-menu/right-sub-menu';
 import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with-subtitle';
+import {Verse} from '@core/verse';
+import {gen_1_28_31} from '@core/books/Genesis/gen_1';
+import {gen_2_1_25} from '@core/books/Genesis/gen_2';
+import {gen_3_1_19} from '@core/books/Genesis/gen_3';
 
 @Component({
   selector: 'app-innocence',
   imports: [
     SubMenu,
-    RightSubMenu
+    RightSubMenu,
+    Verse
   ],
   templateUrl: './innocence.html',
   styleUrl: '../dispensation.scss',
@@ -15,4 +20,7 @@ import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with
 export class Innocence extends BasePageWithSubtitle {
     override pageSubtitle = "Dispensation of the Innocence";
 
+  protected readonly gen_1_28_31 = gen_1_28_31;
+  protected readonly gen_2_1_25 = gen_2_1_25;
+  protected readonly gen_3_1_19 = gen_3_1_19;
 }
