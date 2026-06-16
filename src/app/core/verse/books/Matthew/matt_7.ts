b4601c964925cf9b5ebe55f821901b273c306d46
DeanN60 <dneighbour54@gmail.com>
2026-06-16 15:10:36 -0400

    add matthew verses

--- src/app/core/verse/books/Matthew/matt_7.ts
+++ src/app/core/verse/books/Matthew/matt_7.ts
@@ -1,5 +1,12 @@
 import {VerseData} from '@core/verse-data';
 
+export const matt_7_13_14 = new VerseData(
+  "Matthew 7:13-14",
+  "13 “Enter through the narrow gate; for the gate is wide and the way is broad that leads to destruction, and there are many who enter through it. " +
+  "14 For the gate is narrow and the way is constricted that leads to life, and there are few who find it.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3262). 316 Publishing, Inc.. Kindle Edition."
+)
+
 export const matt_7_21_23 = new VerseData(
   "Matthew 7:21-23 KJV",
   "21 Not every one that saith unto me, Lord, Lord, shall enter into the kingdom of heaven; but he that doeth the will of my Father which is in heaven. " +
