fb67f8d6ada933cf379e3808af88f6e35d4358bf
DeanN60 <dneighbour54@gmail.com>
2026-06-16 09:04:06 -0400

    add quote from Against Heresies

--- src/app/feature/theology/eschatology/pre-trib/pre-trib.ts
+++ src/app/feature/theology/eschatology/pre-trib/pre-trib.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import {TheologySubMenu} from '../../theology-sub-menu/theology-sub-menu';
 import {EschatologyRightMenu} from "../eschatology-right-menu/eschatology-right-menu";
 import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with-subtitle';
+import {Verse} from '@core/verse';
+import {againstHereChap29p1} from '@core/books/Against Heresies/Book 5/chapter_29';
 
 @Component({
   selector: 'app-pre-trib',
   imports: [
     TheologySubMenu,
-    EschatologyRightMenu
+    EschatologyRightMenu,
+    Verse
   ],
   templateUrl: './pre-trib.html',
   styleUrl: '../eschatology.scss',
@@ -15,4 +18,5 @@ import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with
 export class PreTrib extends BasePageWithSubtitle {
     override pageSubtitle = "Pre-Tribulation";
 
+  protected readonly againstHereChap29p1 = againstHereChap29p1;
 }
