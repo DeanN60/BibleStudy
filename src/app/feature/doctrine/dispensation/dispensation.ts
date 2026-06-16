0b147e3c4e92629ffe9766b67eaf54d0ef3ee284
DeanN60 <dneighbour54@gmail.com>
2026-06-16 15:09:27 -0400

    add references to dispensation

--- src/app/feature/doctrine/dispensation/dispensation.ts
+++ src/app/feature/doctrine/dispensation/dispensation.ts
@@ -7,13 +7,20 @@ import {eph_1_10} from '@core/books/Ephesians/eph_1';
 import {eph_3_2} from '@core/books/Ephesians/eph_3';
 import {col_1_25} from '@core/books/Colossians/col_1';
 import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with-subtitle';
+import {Definition, iDefinition} from '@core/definition/definition';
+import {
+  dispensation_1_18,
+  dispensation_1_19,
+  dispensation_1_7
+} from '@core/books/Dispensationalism Before Darby/chap_1';
 
 @Component({
   selector: 'app-dispensation',
   imports: [
     SubMenu,
     RightSubMenu,
-    Verse
+    Verse,
+    Definition
   ],
   templateUrl: './dispensation.html',
   styleUrl: './dispensation.scss',
@@ -21,8 +28,21 @@ import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with
 export class Dispensation extends BasePageWithSubtitle {
   override pageSubtitle = "Dispensation";
 
+  dispensationalismDefinition = {
+    source: "Merriam-Webster",
+    term: "Dispensationalism",
+    type: "noun",
+    definitions: [
+      {def: "adherence to or advocacy of a system of interpreting history in terms of a series of God's dispensations"}
+    ]
+  } as iDefinition;
+
+
   protected readonly iCor_9_17 = iCor_9_17;
   protected readonly eph_1_10 = eph_1_10;
   protected readonly eph_3_2 = eph_3_2;
   protected readonly col_1_25 = col_1_25;
+  protected readonly dispensation_1_7 = dispensation_1_7;
+  protected readonly dispensation_1_18 = dispensation_1_18;
+  protected readonly dispensation_1_19 = dispensation_1_19;
 }
