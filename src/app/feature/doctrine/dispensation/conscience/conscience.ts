02a7e6c37ddd8bbe447576196b6a3165e9b1941d
DeanN60 <dneighbour54@gmail.com>
2026-06-16 15:13:02 -0400

    add references

--- src/app/feature/doctrine/dispensation/conscience/conscience.ts
+++ src/app/feature/doctrine/dispensation/conscience/conscience.ts
@@ -2,12 +2,16 @@ import {Component} from '@angular/core';
 import {SubMenu} from "../../doctrine-sub-menu/sub-menu";
 import {RightSubMenu} from '../right-sub-menu/right-sub-menu';
 import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with-subtitle';
+import {Verse} from '@core/verse';
+import {gen_3_23_24} from '@core/books/Genesis/gen_3';
+import {gen_4_17_22, gen_4_1_8, gen_4_23_24, gen_4_25_26, gen_4_9_16} from '@core/books/Genesis/gen_4';
 
 @Component({
   selector: 'app-conscience',
   imports: [
     SubMenu,
-    RightSubMenu
+    RightSubMenu,
+    Verse
   ],
   templateUrl: './conscience.html',
   styleUrl: '../dispensation.scss',
@@ -15,4 +19,10 @@ import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with
 export class Conscience extends BasePageWithSubtitle {
   override pageSubtitle = "Dispensation of Conscience";
 
+  protected readonly gen_3_23_24 = gen_3_23_24;
+  protected readonly gen_4_1_8 = gen_4_1_8;
+  protected readonly gen_4_9_16 = gen_4_9_16;
+  protected readonly gen_4_17_22 = gen_4_17_22;
+  protected readonly gen_4_23_24 = gen_4_23_24;
+  protected readonly gen_4_25_26 = gen_4_25_26;
 }
