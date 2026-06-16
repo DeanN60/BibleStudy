0631c32d0b921773a3aa3ec6cf8835772710d30c
DeanN60 <dneighbour54@gmail.com>
2026-06-16 15:10:08 -0400

    add dispensation quotes

---
+++ src/app/core/verse/books/Dispensationalism Before Darby/chap_1.ts
@@ -0,0 +1,19 @@
+import {VerseData} from '@core/verse-data';
+
+export const dispensation_1_7 = new VerseData(
+  "Dispensationalism Before Darby Chap 1, Par 7",
+  "Dispensationalism, the most common school of futurist premillennialism, is based on a literal reading of prophetic passages in the Bible and divides history into periods (dispensations) wherein the relationship between God and humanity changes.",
+  "Watson, William C.. Dispensationalism before Darby: Seventeenth-Century and Eighteenth-Century English Apocalypticism (p. 3). Lampion Press. Kindle Edition."
+);
+
+export const dispensation_1_18 = new VerseData(
+  "Dispensationalism Before Darby Chap 1, Par 18",
+  "When Justin Martyr was asked, around A.D. 150, whether he believed that “Jerusalem … shall be rebuilt; and [whether he expected his people] to be gathered together, and made joyful with Christ and the patriarchs,” he responded, “I and many others are of this opinion.”",
+  "Watson, William C.. Dispensationalism before Darby: Seventeenth-Century and Eighteenth-Century English Apocalypticism (p. 5). Lampion Press. Kindle Edition."
+);
+
+export const dispensation_1_19 = new VerseData(
+  "Dispensationalism Before Darby Chap 1, Par 19",
+  "Similarly, around A.D. 170, Irenaeus claimed it was an “indispensable part of orthodoxy to believe that these things shall indeed come to pass on this earth.” Around A.D. 180, Tertullian claimed that he saw the heavenly Jerusalem about to descend to earth and that Christians should “expect Christ to appear in Jerusalem.” Some early Christian sources—notably, The Shepherd of Hermas, Pseudo-Ephraem, and The Apocalypse of Elijah—even hint at a pretribulational rapture.",
+  "Watson, William C.. Dispensationalism before Darby: Seventeenth-Century and Eighteenth-Century English Apocalypticism (p. 5). Lampion Press. Kindle Edition."
+)
