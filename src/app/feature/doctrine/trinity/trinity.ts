1802eced8cb25810e05b144259d4985bd988a1eb
DeanN60 <dneighbour54@gmail.com>
2026-06-16 15:11:54 -0400

    add references to trinity page

--- src/app/feature/doctrine/trinity/trinity.ts
+++ src/app/feature/doctrine/trinity/trinity.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import {SubMenu} from "../doctrine-sub-menu/sub-menu";
 import {Verse} from '@core/verse';
-import {john_1} from '@core/books/John/john_1';
+import {john_1_1_5, john_1_14} from '@core/books/John/john_1';
 import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with-subtitle';
+import {psalms_33_6} from '@core/books/Psalms/psalms_33';
+import {john_14_16_17, john_14_25_26, john_14_9_14} from '@core/books/John/john_14';
 
 @Component({
   selector: 'app-trinity',
@@ -16,5 +18,10 @@ import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with
 export class Trinity extends BasePageWithSubtitle {
   override pageSubtitle = "Trinity";
 
-  protected readonly john_1_1_5 = john_1;
+  protected readonly john_1_1_5 = john_1_1_5;
+  protected readonly john_1_14 = john_1_14;
+  protected readonly psalms_33_6 = psalms_33_6;
+  protected readonly john_14_9_14 = john_14_9_14;
+  protected readonly john_14_16_17 = john_14_16_17;
+  protected readonly john_14_25_26 = john_14_25_26;
 }
