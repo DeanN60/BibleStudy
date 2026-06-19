fc7eab486b5dd2a956b331a7bc9454af6d4d77e8
DeanN60 <dneighbour54@gmail.com>
2026-06-19 06:33:34 -0400

    add verses to

--- src/app/feature/doctrine/dispensation/law/law.ts
+++ src/app/feature/doctrine/dispensation/law/law.ts
@@ -2,12 +2,16 @@ import {Component} from '@angular/core';
 import {SubMenu} from "../../doctrine-sub-menu/sub-menu";
 import {RightSubMenu} from '../right-sub-menu/right-sub-menu';
 import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with-subtitle';
+import {Verse} from '@core/verse';
+import {exodus_19_3_8} from '@core/books/Exodus/exodus_19';
+import {john_19_30} from '@core/books/John/john_19';
 
 @Component({
   selector: 'app-law',
   imports: [
     SubMenu,
-    RightSubMenu
+    RightSubMenu,
+    Verse
   ],
   templateUrl: './law.html',
   styleUrl: '../dispensation.scss',
@@ -15,4 +19,6 @@ import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with
 export class Law extends BasePageWithSubtitle {
   override pageSubtitle = "Dispensation of the Law";
 
+  protected readonly exodus_19_3_8 = exodus_19_3_8;
+  protected readonly john_19_30 = john_19_30;
 }
