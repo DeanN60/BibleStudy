ed031c2ed3a6bdc831ffc090ee72578193eefe7d
DeanN60 <dneighbour54@gmail.com>
2026-06-19 11:11:07 -0400

    add home routes adjust home to have a menu

--- src/app/feature/home/home.ts
+++ src/app/feature/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {HomeSubMenu} from './home-sub-menu/home-sub-menu';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    HomeSubMenu
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
