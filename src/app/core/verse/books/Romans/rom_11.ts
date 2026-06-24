6af8cfb2b53e06512ad301e3b4f48f937950d93e
DeanN60 <dneighbour54@gmail.com>
2026-06-24 15:00:42 -0400

    add rom_11

--- src/app/core/verse/books/Romans/rom_11.ts
+++ src/app/core/verse/books/Romans/rom_11.ts
@@ -5,3 +5,16 @@ export const rom_11_6 = new VerseData(
   "6 And if by grace, then is it no more of works: otherwise grace is no more grace. But if it be of works, then is it no more grace: otherwise work is no more work.",
   "King James Version"
 );
+
+export const rom_11_17_24 = new VerseData(
+  "Romans 11:17-24 LSB",
+  "17 But if some of the branches were broken off, and you, being a wild olive, were grafted in among them and became a partaker with them of the rich root of the olive tree, " +
+  "18 do not boast against the branches. But if you do boast against them, remember that it is not you who supports the root, but the root supports you. " +
+  "19 You will say then, “Branches were broken off so that I might be grafted in.” " +
+  "20 Quite right! They were broken off for their unbelief, but you stand by your faith. Do not be haughty, but fear, " +
+  "21 for if God did not spare the natural branches, He will not spare you, either. " +
+  "22 Behold then the kindness and severity of God; to those who fell, severity, but to you, God’s kindness, if you continue in His kindness; otherwise you also will be cut off. " +
+  "23 And they also, if they do not continue in their unbelief, will be grafted in, for God is able to graft them in again. " +
+  "24 For if you were cut off from what is by nature a wild olive tree, and were grafted contrary to nature into a cultivated olive tree, how much more will these who are the natural branches be grafted into their own olive tree?",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3790). 316 Publishing, Inc.. Kindle Edition."
+);
