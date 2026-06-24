fd7542d57c51d9dafa54a6d5337f4bb8b48c3e7f
DeanN60 <dneighbour54@gmail.com>
2026-06-24 15:06:56 -0400

    add Jesus ministry

--- src/app/feature/jesus/ministry/ministry.ts
+++ src/app/feature/jesus/ministry/ministry.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {JesusSubMenu} from '../jesus-sub-menu/jesus-sub-menu';
 import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with-subtitle';
+import {RightSubMenu} from './right-sub-menu/right-sub-menu';
 
 @Component({
   selector: 'app-ministry',
   imports: [
-    JesusSubMenu
+    JesusSubMenu,
+    RightSubMenu
   ],
   templateUrl: './ministry.html',
   styleUrl: '../jesus.scss',
