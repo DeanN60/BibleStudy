39a8a8ec0d4147eb1efee6e4cb5e6ef7e41d4450
DeanN60 <dneighbour54@gmail.com>
2026-06-27 07:25:10 -0400

    add verse to post-trib

--- src/app/feature/theology/eschatology/post-trib/post-trib.ts
+++ src/app/feature/theology/eschatology/post-trib/post-trib.ts
@@ -5,6 +5,7 @@ import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with
 import {Verse} from '@core/verse';
 import {rom_11_17_24} from '@core/books/Romans/rom_11';
 import {john_15_1_11} from '@core/books/John/john_15';
+import {matt_25_41_45} from '@core/books/Matthew/matt_25';
 
 @Component({
   selector: 'app-post-trib',
@@ -21,4 +22,5 @@ export class PostTrib extends BasePageWithSubtitle {
 
   protected readonly rom_11_17_24 = rom_11_17_24;
   protected readonly john_15_1_11 = john_15_1_11;
+  protected readonly matt_25_41_45 = matt_25_41_45;
 }
