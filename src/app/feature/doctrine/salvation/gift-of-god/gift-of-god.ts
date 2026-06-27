09a444f8a2ba7c7a7c065964898d548748ec7e1f
DeanN60 <dneighbour54@gmail.com>
2026-06-27 15:02:10 -0400

    clean up

--- src/app/feature/doctrine/salvation/gift-of-god/gift-of-god.ts
+++ src/app/feature/doctrine/salvation/gift-of-god/gift-of-god.ts
@@ -4,6 +4,7 @@ import {RightSubMenu} from '../right-sub-menu/right-sub-menu';
 import {Verse} from '@core/verse';
 import {eph_2_8} from '@core/books/Ephesians/eph_2';
 import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with-subtitle';
+import {john_14_6} from '@core/books/John/john_14';
 
 @Component({
   selector: 'app-gift-of-god',
@@ -19,4 +20,5 @@ export class GiftOfGod extends BasePageWithSubtitle {
   readonly pageSubtitle = "Gift of God"
 
   protected readonly eph_2_8 = eph_2_8;
+  protected readonly john_14_6 = john_14_6;
 }
