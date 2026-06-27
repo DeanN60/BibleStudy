652877126a8cf4f0813769b30212ae67c5b3af19
DeanN60 <dneighbour54@gmail.com>
2026-06-27 09:32:00 -0400

    add trade

--- src/app/feature/jesus/disciples/matthew/matthew.ts
+++ src/app/feature/jesus/disciples/matthew/matthew.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with-subtitle';
 import {JesusSubMenu} from '../../jesus-sub-menu/jesus-sub-menu';
 import {RightSubMenu} from '../right-sub-menu/right-sub-menu';
+import {Verse} from '@core/verse';
+import {matt_9_9} from '@core/books/Matthew/matt_9';
 
 @Component({
   selector: 'app-matthew',
   imports: [
     JesusSubMenu,
-    RightSubMenu
+    RightSubMenu,
+    Verse
   ],
   templateUrl: './matthew.html',
   styleUrl: '../disciples.scss',
@@ -15,4 +18,5 @@ import {RightSubMenu} from '../right-sub-menu/right-sub-menu';
 export class Matthew extends BasePageWithSubtitle {
     override pageSubtitle = "Disciples - Matthew";
 
+  protected readonly matt_9_9 = matt_9_9;
 }
