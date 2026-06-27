a4069350916bef3e6d25581eec46bad32082a8b1
DeanN60 <dneighbour54@gmail.com>
2026-06-27 05:41:35 -0400

    add jacobs-trouble

--- src/app/feature/theology/eschatology/jacobs-trouble/jacobs-trouble.ts
+++ src/app/feature/theology/eschatology/jacobs-trouble/jacobs-trouble.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import {TheologySubMenu} from '../../theology-sub-menu/theology-sub-menu';
 import {RightSubMenu} from '../right-sub-menu/right-sub-menu';
 import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with-subtitle';
+import {Verse} from '@core/verse';
+import {jer_30_10_11, jer_30_1_6, jer_30_7} from '@core/books/Jeremiah/jer_30';
+import {matt_24_21} from '@core/books/Matthew/matt_24';
 
 @Component({
   selector: 'app-jacobs-trouble',
   imports: [
     TheologySubMenu,
-    RightSubMenu
+    RightSubMenu,
+    Verse
   ],
   templateUrl: './jacobs-trouble.html',
   styleUrl: '../eschatology.scss',
@@ -15,4 +19,8 @@ import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with
 export class JacobsTrouble extends BasePageWithSubtitle {
     override pageSubtitle = "Jacob`s Trouble";
 
+  protected readonly jer_30_7 = jer_30_7;
+  protected readonly jer_30_1_6 = jer_30_1_6;
+  protected readonly jer_30_10_11 = jer_30_10_11;
+  protected readonly matt_24_21 = matt_24_21;
 }
