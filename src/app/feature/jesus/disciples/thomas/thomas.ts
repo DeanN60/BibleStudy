395006b5d4e87b57995ebd2f7d781468165f29d4
DeanN60 <dneighbour54@gmail.com>
2026-06-27 09:33:35 -0400

    add verses

--- src/app/feature/jesus/disciples/thomas/thomas.ts
+++ src/app/feature/jesus/disciples/thomas/thomas.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with-subtitle';
 import {JesusSubMenu} from '../../jesus-sub-menu/jesus-sub-menu';
 import {RightSubMenu} from '../right-sub-menu/right-sub-menu';
+import {Verse} from '@core/verse';
+import {matt_10_2_4} from '@core/books/Matthew/matt_10';
 
 @Component({
   selector: 'app-thomas',
   imports: [
     JesusSubMenu,
-    RightSubMenu
+    RightSubMenu,
+    Verse
   ],
   templateUrl: './thomas.html',
   styleUrl: '../disciples.scss',
@@ -15,4 +18,5 @@ import {RightSubMenu} from '../right-sub-menu/right-sub-menu';
 export class Thomas extends BasePageWithSubtitle {
     override pageSubtitle = "Thomas - Thomas";
 
+  protected readonly matt_10_2_4 = matt_10_2_4;
 }
