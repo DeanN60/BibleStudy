dca06334ebaf2589673ae77e8ffe79c4276a16bb
DeanN60 <dneighbour54@gmail.com>
2026-06-27 07:26:59 -0400

    update disciples

--- src/app/feature/jesus/disciples/disciples.ts
+++ src/app/feature/jesus/disciples/disciples.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
 import {JesusSubMenu} from '../jesus-sub-menu/jesus-sub-menu';
 import {RightSubMenu} from './right-sub-menu/right-sub-menu';
+import {Verse} from '@core/verse';
+import {matt_4_18_22} from '@core/books/Matthew/matt_4';
+import {mark_1_16_20} from '@core/books/Mark/mark_1';
+import {luke_5_1_11} from '@core/books/Luke/luke_5';
+import {john_1_35_51} from '@core/books/John/john_1';
+import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with-subtitle';
 
 @Component({
   selector: 'app-disciples',
   imports: [
     JesusSubMenu,
-    RightSubMenu
+    RightSubMenu,
+    Verse
   ],
   templateUrl: './disciples.html',
-  styleUrl: '../ministry/ministry.scss',
+  styleUrl: './disciples.scss',
 })
-export class Disciples {
+export class Disciples extends BasePageWithSubtitle {
+  override pageSubtitle = "Disciples";
 
+  protected readonly matt_4_18_22 = matt_4_18_22;
+  protected readonly mark_1_16_20 = mark_1_16_20;
+  protected readonly luke_5_1_11 = luke_5_1_11;
+  protected readonly john_1_35_51 = john_1_35_51;
 }
