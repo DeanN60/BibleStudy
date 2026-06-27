aa8880c4cdee92cd62af06a30406e8ea8311f54b
DeanN60 <dneighbour54@gmail.com>
2026-06-27 15:01:55 -0400

    add text and verses

--- src/app/feature/doctrine/salvation/need-for-salvation/need-for-salvation.ts
+++ src/app/feature/doctrine/salvation/need-for-salvation/need-for-salvation.ts
@@ -4,6 +4,8 @@ import {RightSubMenu} from '../right-sub-menu/right-sub-menu';
 import {Verse} from '@core/verse';
 import {rom_3_23_26} from '@core/books/Romans/rom_3';
 import {BasePageWithSubtitle} from '@core/base-page-with-subtitle/base-page-with-subtitle';
+import {john_14_6} from '@core/books/John/john_14';
+import {isaiah_53_5} from '@core/books/Isaiah/isaiah_53';
 
 @Component({
   selector: 'app-need-for-salvation',
@@ -19,4 +21,6 @@ export class NeedForSalvation extends BasePageWithSubtitle {
   override pageSubtitle = "Need For Salvation";
 
   protected readonly rom_3_23_26 = rom_3_23_26;
+  protected readonly john_14_6 = john_14_6;
+  protected readonly isaiah_53_5 = isaiah_53_5;
 }
