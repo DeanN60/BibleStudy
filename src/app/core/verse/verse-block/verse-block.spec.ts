e213254b27ef87774e1b768283e2762442270e55
DeanN60 <dneighbour54@gmail.com>
2026-06-27 07:26:01 -0400

    fix test

--- src/app/core/verse/verse-block/verse-block.spec.ts
+++ src/app/core/verse/verse-block/verse-block.spec.ts
@@ -1,6 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { VerseBlock } from './verse-block';
+import {VerseBlock} from './verse-block';
+import {provideRouter} from '@angular/router';
+import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClientTesting} from '@angular/common/http/testing';
+import {provideZonelessChangeDetection} from '@angular/core';
+import {matt_24_21} from '@core/books/Matthew/matt_24';
 
 describe('VerseBlock', () => {
   let component: VerseBlock;
@@ -8,12 +13,20 @@ describe('VerseBlock', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [VerseBlock]
-    })
-    .compileComponents();
+      imports: [
+        VerseBlock
+      ],
+      providers: [
+        provideRouter([]),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideZonelessChangeDetection()
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(VerseBlock);
     component = fixture.componentInstance;
+    component.data.apply(matt_24_21);
     await fixture.whenStable();
   });
 
