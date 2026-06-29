0fd28afec823959bf0e388d9d6cd6b5c8dca47ff
DeanN60 <dneighbour54@gmail.com>
2026-06-29 11:03:43 -0400

    update rom_4 update faith-and-works update not-of-works

--- src/app/core/verse/books/Romans/rom_4.ts
+++ src/app/core/verse/books/Romans/rom_4.ts
@@ -1,13 +1,26 @@
 import {VerseData} from '@core/verse-data';
 
+export const rom_4_1_8 = new VerseData(
+  "Romans 4:1-8 LSB",
+  "1 What then shall we say that Abraham, our forefather according to the flesh, has found? " +
+  "2 For if Abraham was justified by works, he has something to boast about—but not before God! " +
+  "3 For what does the Scripture say? “ABRAHAM BELIEVED GOD, AND IT WAS COUNTED TO HIM AS RIGHTEOUSNESS.” " +
+  "4 Now to the one who works, his wage is not counted according to grace, but according to what is due. " +
+  "5 But to the one who does not work, but believes upon Him who justifies the ungodly, his faith is counted as righteousness, " +
+  "6 just as David also speaks of the blessing on the man to whom God counts righteousness apart from works: " +
+  "7 “BLESSED ARE THOSE WHOSE LAWLESS DEEDS HAVE BEEN FORGIVEN, AND WHOSE SINS HAVE BEEN COVERED. " +
+  "8 BLESSED IS THE MAN WHOSE SIN THE aLORD WILL NOT TAKE INTO ACCOUNT.”",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3774). 316 Publishing, Inc.. Kindle Edition."
+);
+
 export const rom_4_4 = new VerseData(
-  "Romans 4:4 KJV",
-  "4 Now to him that worketh is the reward not reckoned of grace, but of debt.",
-  "King James Version"
+  "Romans 4:4 LSB",
+  "4 Now to the one who works, his wage is not counted according to grace, but according to what is due.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3774). 316 Publishing, Inc.. Kindle Edition."
 );
 
 export const rom_4_5 = new VerseData(
-  "Romans 4:5 KJV",
-  "5 But to him that worketh not, but believeth on him that justifieth the ungodly, his faith is counted for righteousness.",
-  "King James Version"
+  "Romans 4:5 LSB",
+  "5 But to the one who does not work, but believes upon Him who justifies the ungodly, his faith is counted as righteousness,",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3774). 316 Publishing, Inc.. Kindle Edition."
 );
