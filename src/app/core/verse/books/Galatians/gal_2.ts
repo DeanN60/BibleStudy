16688f1924af9d27b039ba4aec186d7ab6af53d5
DeanN60 <dneighbour54@gmail.com>
2026-06-29 10:35:23 -0400

    add verse

---
+++ src/app/core/verse/books/Galatians/gal_2.ts
@@ -0,0 +1,13 @@
+import {VerseData} from '@core/verse-data';
+
+export const gal_2_15_21 = new VerseData(
+  "Galatians 2:15-21 LSB",
+  "15 “We are Jews by nature and not sinners from among the Gentiles; " +
+  "16 nevertheless knowing that a man is not justified by the works of the Law but through faith in Jesus Christ, even we have believed in Christ Jesus, so that we may be justified by faith in Christ and not by the works of the Law; since by the works of the Law no flesh will be justified. " +
+  "17 But if, while seeking to be justified in Christ, we ourselves have also been found sinners, is Christ then a minister of sin? May it never be! " +
+  "18 For if I rebuild what I have once destroyed, I prove myself to be a transgressor. " +
+  "19 For through the Law I died to the Law, so that I might live to God. " +
+  "20 I have been crucified with Christ, and it is no longer I who live, but Christ lives in me. And the life which I now live in the flesh I live by faith in the Son of God, who loved me and gave Himself up for me. " +
+  "21 I do not set aside the grace of God, for if righteousness comes through athe Law, then Christ died needlessly.”",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3899). 316 Publishing, Inc.. Kindle Edition."
+);
---
+++ src/app/core/verse/books/James/james_2.ts
@@ -0,0 +1,15 @@
+import {VerseData} from '@core/verse-data';
+
+export const james_2_18_26 = new VerseData(
+  "James 2:18-26 LSB",
+  "18 But someone will say, “You have faith; and I have works. Show me your faith without the works, and I will show you my faith by my works.” " +
+  "19 You believe that God is one. You do well; the demons also believe, and shudder. " +
+  "20 But are you willing to recognize, you foolish fellow, that faith without works is useless? " +
+  "21 Was not Abraham our father justified by works when he offered up Isaac his son on the altar? " +
+  "22 You see that faith was working with his works, and as a result of the works, faith was perfected. " +
+  "23 And the Scripture was fulfilled which says, “AND ABRAHAM BELIEVED GOD, AND IT WAS COUNTED TO HIM AS RIGHTEOUSNESS,” and he was called the friend of God. " +
+  "24 You see that a man is justified by works and not by faith alone. " +
+  "25 And in the same way, was not Rahab the harlot also justified by works when she received the messengers and sent them out by another way? " +
+  "26 For just as the body without the spirit is dead, so also faith without works is dead.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 4055). 316 Publishing, Inc.. Kindle Edition."
+);
