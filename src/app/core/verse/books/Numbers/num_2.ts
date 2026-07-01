c06a9fc2b4be257fe9bec011f45fda453e9eb7d7
DeanN60 <dneighbour54@gmail.com>
2026-07-01 09:02:40 -0400

    add verse

---
+++ src/app/core/verse/books/Numbers/num_2.ts
@@ -0,0 +1,50 @@
+import {VerseData} from '@core/verse-data';
+
+export const num_2_3_9 = new VerseData(
+  "Numbers 2:3-9 LSB",
+  "3 Now those who camp on the east side toward the sunrise shall be of the standard of the camp of Judah, by their armies, and the leader of the sons of Judah: Nahshon the son of Amminadab, " +
+  "4 and his army, even their numbered men, 74,600. " +
+  "5 Those who camp next to him shall be the tribe of Issachar, and the leader of the sons of Issachar: Nethanel the son of Zuar, " +
+  "6 and his army, even his numbered men, 54,400. " +
+  "7 Then the tribe of Zebulun, and the leader of the sons of Zebulun: Eliab the son of Helon, " +
+  "8 and his army, even his numbered men, 57,400. " +
+  "9 The total of the numbered men of the camp of Judah: 186,400, by their armies. They shall set out first.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 550). 316 Publishing, Inc.. Kindle Edition. "
+);
+
+export const num_2_10_16 = new VerseData(
+  "Numbers 2:10-16 LSB",
+  "10 “On the south side shall be the standard of the camp of Reuben by their armies, and the leader of the sons of Reuben: Elizur the son of Shedeur, " +
+  "11 and his army, even his numbered men, 46,500. " +
+  "12 And those who camp next to him shall be the tribe of Simeon, and the leader of the sons of Simeon: Shelumiel the son of Zurishaddai, " +
+  "13 and his army, even their numbered men, 59,300. " +
+  "14 Then the tribe of Gad, and the leader of the sons of Gad: Eliasaph the son of Deuel, " +
+  "15 and his army, even their numbered men, 45,650. " +
+  "16 The total of the numbered men of the camp of Reuben: 151,450 by their armies. And they shall set out second.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 550). 316 Publishing, Inc.. Kindle Edition. "
+);
+
+export const num_2_17_24 = new VerseData(
+  "Numbers 2:17-24 LSB",
+  "17 “Then the tent of meeting shall set out with the camp of the Levites in the midst of the camps; just as they camp, so they shall set out, every man in his place by their standards. " +
+  "18 “On the west side shall be the standard of the camp of Ephraim by their armies, and the leader of the sons of Ephraim: Elishama the son of Ammihud, " +
+  "19 and his army, even their numbered men, 40,500. " +
+  "20 Next to him shall be the tribe of Manasseh, and the leader of the sons of Manasseh: Gamaliel the son of Pedahzur, " +
+  "21 and his army, even their numbered men, 32,200. " +
+  "22 Then the tribe of Benjamin, and the leader of the sons of Benjamin: Abidan the son of Gideoni, " +
+  "23 and his army, even their numbered men, 35,400. " +
+  "24 The total of the numbered men of the camp of Ephraim: 108,100, by their armies. And they shall set out third.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 550). 316 Publishing, Inc.. Kindle Edition. "
+);
+
+export const num_2_25_31 = new VerseData(
+  "Numbers 2:25-31 LSB",
+  "25 “On the north side shall be the standard of the camp of Dan by their armies, and the leader of the sons of Dan: Ahiezer the son of Ammishaddai, " +
+  "26 and his army, even their numbered men, 62,700. " +
+  "27 Those who camp next to him shall be the tribe of Asher, and the leader of the sons of Asher: Pagiel the son of Ochran, " +
+  "28 and his army, even their numbered men, 41,500. " +
+  "29 Then the tribe of Naphtali, and the leader of the sons of Naphtali: Ahira the son of Enan, " +
+  "30 and his army, even their numbered men, 53,400. " +
+  "31 The total of the numbered men of the camp of Dan was 157,600. They shall set out last by their standards.”",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 550). 316 Publishing, Inc.. Kindle Edition. "
+);
