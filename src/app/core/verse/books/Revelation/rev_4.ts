a1ccab1fff29d1260b5079d16e29805f56d4f9d8
DeanN60 <dneighbour54@gmail.com>
2026-07-01 09:43:20 -0400

    add verse

---
+++ src/app/core/verse/books/Revelation/rev_4.ts
@@ -0,0 +1,8 @@
+import {VerseData} from '@core/verse-data';
+
+export const rev_4_6_7 = new VerseData(
+  "Revelation 4:6,7 LSB",
+  "6 And before the throne there was something like a sea of glass, like crystal. And in the center and around the throne, four living creatures full of eyes in front and behind. " +
+  "7 And the first creature was like a lion, and the second creature like a calf, and the third creature had a face like that of a man, and the fourth creature was like a flying eagle.",
+"Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 4118). 316 Publishing, Inc.. Kindle Edition."
+);
