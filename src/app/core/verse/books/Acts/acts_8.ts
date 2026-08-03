4945b97feaa001f76f3bf1ff84d21e14be6c2646
DeanN60 <dneighbour54@gmail.com>
2026-08-03 11:42:21 -0400

    add acts_8 verse

---
+++ src/app/core/verse/books/Acts/acts_8.ts
@@ -0,0 +1,8 @@
+import {VerseData} from '@core/verse-data';
+
+export const acts_8_39_40 = new VerseData(
+  "Acts 8:39-40 LSB",
+  "39 When they came up out of the water, the Spirit of the Lord snatched Philip away, and the eunuch no longer saw him, abut went on his way rejoicing. " +
+  "40 But Philip found himself at Azotus, and as he passed through he kept proclaiming the gospel to all the cities until he came to Caesarea.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3668). 316 Publishing, Inc.. Kindle Edition. "
+);
