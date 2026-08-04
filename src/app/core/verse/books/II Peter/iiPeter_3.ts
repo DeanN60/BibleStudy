d23feaff008cc0b06c6f06fbb12013067c5f6182
DeanN60 <dneighbour54@gmail.com>
2026-08-04 12:02:16 -0400

    add verses

--- src/app/core/verse/books/II Peter/iiPeter_3.ts
+++ src/app/core/verse/books/II Peter/iiPeter_3.ts
@@ -11,3 +11,9 @@ export const iiPeter_3_10 = new VerseData(
   "10 But the day of the Lord will come like a thief, in which the heavens will pass away with a roar and the elements will be destroyed with intense heat, and the earth and its works will be found out.",
   "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 4086). 316 Publishing, Inc.. Kindle Edition."
 );
+
+export const iiPeter_3_13 = new VerseData(
+  "II Peter 3:13 LSB",
+  "13 But according to His promise we are looking for NEW HEAVENS AND A NEW EARTH, in which righteousness dwells.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 4086). 316 Publishing, Inc.. Kindle Edition."
+);
---
+++ src/app/core/verse/books/Isaiah/isaiah_13.ts
@@ -0,0 +1,13 @@
+import {VerseData} from '@core/verse-data';
+
+export const isaiah_13_6 = new VerseData(
+  "Isaiah 13:6 LSB",
+  "6 Wail, for the day of Yahweh is near! It will come as destruction from the Almighty. ",
+"Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 2376). 316 Publishing, Inc.. Kindle Edition. "
+);
+
+export const isaiah_13_9 = new VerseData(
+  "Isaiah 13:9 LSB",
+  "9 Behold, the day of Yahweh is coming, Cruel, with fury and burning anger, To make the land a desolation; And He will exterminate its sinners from it.",
+"Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 2376). 316 Publishing, Inc.. Kindle Edition. "
+);
