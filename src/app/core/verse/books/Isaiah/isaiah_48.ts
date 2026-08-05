d54f35f6d4d862d3ad30b3d901901b952e4db10c
DeanN60 <dneighbour54@gmail.com>
2026-08-05 11:13:36 -0400

    update five solas

---
+++ src/app/core/verse/books/Isaiah/isaiah_48.ts
@@ -0,0 +1,7 @@
+import {VerseData} from '@core/verse-data';
+
+export const isaiah_48_11 = new VerseData(
+  "Isaiah 48:11 LSB",
+  "11 For My own sake, for My own sake, I will act; For how can My name be profaned? And My glory I will not give to another.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 2477). 316 Publishing, Inc.. Kindle Edition."
+);
---
+++ src/app/core/verse/books/Titus/titus_3.ts
@@ -0,0 +1,8 @@
+import {VerseData} from '@core/verse-data';
+
+export const titus_3_4_5 = new VerseData(
+  "Titus 3:4-5 LSB",
+  "4 But when the kindness and affection of God our Savior appeared, " +
+  "5 He saved us, not by works which we did in righteousness, but according to His mercy, through the washing of regeneration and renewing by the Holy Spirit,",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 4007). 316 Publishing, Inc.. Kindle Edition."
+);
