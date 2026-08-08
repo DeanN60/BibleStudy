15a3013d997235aa3fc8e65e4b1c1672be62ff07
DeanN60 <dneighbour54@gmail.com>
2026-08-08 09:46:57 -0400

    add appKeepInViewport to book

--- src/app/core/book/book.ts
+++ src/app/core/book/book.ts
@@ -1,11 +1,13 @@
 import {Component, input, ChangeDetectionStrategy} from '@angular/core';
 import {BookData, fallbackBookData} from '@core/book-data';
 import {OpenTrigger} from '@core/directive/open-trigger';
+import {KeepInViewport} from '@core/directive/keep-in-viewport';
 
 @Component({
   selector: 'app-book',
   imports: [
-    OpenTrigger
+    OpenTrigger,
+    KeepInViewport
   ],
   templateUrl: './book.html',
   changeDetection: ChangeDetectionStrategy.Eager,
