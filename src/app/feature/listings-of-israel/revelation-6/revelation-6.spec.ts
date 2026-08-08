6525b49b10cd7b3f3fae57e9c1686c2138757589
DeanN60 <dneighbour54@gmail.com>
2026-08-08 10:59:00 -0400

    update test for revelation-6

--- src/app/feature/listings-of-israel/revelation-6/revelation-6.spec.ts
+++ src/app/feature/listings-of-israel/revelation-6/revelation-6.spec.ts
@@ -1,9 +1,21 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-
-import {Revelation6} from './revelation-6';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Revelation6 } from './revelation-6';
+import { LoiSubMenu } from '../loi-sub-menu/loi-sub-menu';
+import { Tribe } from '@core/tribe/tribe';
+import {Component, input} from '@angular/core';
 import {provideRouter} from '@angular/router';
 import {provideHttpClient} from '@angular/common/http';
 import {provideHttpClientTesting} from '@angular/common/http/testing';
+
+// Mock child components if you want to isolate Revelation6 unit testing
+@Component({ selector: 'app-loi-sub-menu', standalone: true, template: '' })
+class MockLoiSubMenu {}
+
+@Component({ selector: 'app-tribe', standalone: true, template: '' })
+class MockTribe {
+  name = input<string>();
+  showStandard = input<boolean>(false);
+}
 
 describe('Revelation6', () => {
   let component: Revelation6;
@@ -18,15 +30,25 @@ describe('Revelation6', () => {
         provideRouter([]),
         provideHttpClient(),
         provideHttpClientTesting()
-      ],
-    }).compileComponents();
+      ]
+    })
+      // Override child components with mocks if you do not want to render full child trees
+      .overrideComponent(Revelation6, {
+        remove: { imports: [LoiSubMenu, Tribe] },
+        add: { imports: [MockLoiSubMenu, MockTribe] }
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(Revelation6);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the pageSubtitle to "Revelation 6 Listing"', () => {
+    expect(component.pageSubtitle).toBe('Revelation 6 Listing');
   });
 });
