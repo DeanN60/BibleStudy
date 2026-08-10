dc922b48debec39f388bcfdd6d4103835fdf7403
DeanN60 <dneighbour54@gmail.com>
2026-08-10 09:58:43 -0400

    update test with external mocks

--- src/app/feature/listings-of-israel/numbers-13/numbers-13.spec.ts
+++ src/app/feature/listings-of-israel/numbers-13/numbers-13.spec.ts
@@ -1,5 +1,4 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {Component, input} from '@angular/core';
 import {beforeEach, describe, expect, it} from 'vitest';
 import {Numbers13} from './numbers-13';
 import {LoiSubMenu} from '../loi-sub-menu/loi-sub-menu';
@@ -7,17 +6,8 @@ import {Tribe} from '@core/tribe/tribe';
 import {provideRouter} from '@angular/router';
 import {provideHttpClient} from '@angular/common/http';
 import {provideHttpClientTesting} from '@angular/common/http/testing';
-
-// Mock child components if needed to isolate the test
-@Component({selector: 'app-loi-sub-menu', standalone: true, template: ''})
-class MockLoiSubMenu {
-}
-
-@Component({selector: 'app-tribe', standalone: true, template: ''})
-class MockTribe {
-  name = input<string>();
-  showStandard = input<boolean>(false);
-}
+import {MockLoiSubMenu} from '@mock/mock-loi-sub-menu';
+import {MockTribe} from '@mock/mock-tribe';
 
 describe('Numbers13', () => {
   let component: Numbers13;
