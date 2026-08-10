e9824410e4a069710fcd55aa6a7c0a0f109820a2
DeanN60 <dneighbour54@gmail.com>
2026-08-10 09:44:42 -0400

    update test with external mocks

--- src/app/feature/doctrine/trinity/trinity.spec.ts
+++ src/app/feature/doctrine/trinity/trinity.spec.ts
@@ -2,42 +2,14 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {beforeEach, describe, expect, it} from 'vitest';
 import {Trinity} from './trinity';
 import {SubMenu} from '../doctrine-sub-menu/sub-menu';
-import {Definition, iDefinition} from '@core/definition/definition';
+import {Definition} from '@core/definition/definition';
 import {Book} from '@core/book/book';
 import {provideRouter} from '@angular/router';
 import {provideHttpClient} from '@angular/common/http';
 import {provideHttpClientTesting} from '@angular/common/http/testing';
-import {Component, input} from '@angular/core';
-import {BookData, fallbackBookData} from '@core/book-data';
-
-// Mock child components to keep unit tests isolated
-@Component({selector: 'app-doctrine-sub-menu', template: ''})
-class MockSubMenu {
-}
-
-@Component({selector: 'app-definition', template: ''})
-class MockDefinition {
-  default = {
-    source: 'unknown',
-    term: 'Test',
-    type: "noun",
-    definitions: [
-      {def: "Test 1"},
-      {def: "Test 2"},
-      {def: "Test 3"}
-    ]
-  } as iDefinition;
-  data = input<iDefinition>(this.default);
-}
-
-@Component({selector: 'app-book', template: ''})
-class MockBook {
-  data = input.required<BookData, BookData | undefined>({
-    transform: (value: BookData | undefined): BookData => {
-      return value ?? fallbackBookData;
-    }
-  });
-}
+import {MockDoctrineSubMenu} from '@mock/mock-doctrine-sub-menu';
+import {MockDefinition} from '@mock/mock-definition';
+import {MockBook} from '@mock/mock-book';
 
 describe('Trinity Component', () => {
   let component: Trinity;
@@ -57,7 +29,7 @@ describe('Trinity Component', () => {
       // Mock child components if needed to isolate the Trinity component test
       .overrideComponent(Trinity, {
         remove: {imports: [SubMenu, Definition, Book]},
-        add: {imports: [MockSubMenu, MockDefinition, MockBook]}
+        add: {imports: [MockDoctrineSubMenu, MockDefinition, MockBook]}
       })
       .compileComponents();
 
@@ -90,37 +62,3 @@ describe('Trinity Component', () => {
     expect(component['john_14_25_26']).toBeDefined();
   });
 });
-
-
-// import {ComponentFixture, TestBed} from '@angular/core/testing';
-//
-// import {Trinity} from './trinity';
-// import {provideRouter} from '@angular/router';
-// import {provideHttpClient} from '@angular/common/http';
-// import {provideHttpClientTesting} from '@angular/common/http/testing';
-//
-// describe('Trinity', () => {
-//   let component: Trinity;
-//   let fixture: ComponentFixture<Trinity>;
-//
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [
-//         Trinity
-//       ],
-//       providers: [
-//         provideRouter([]),
-//         provideHttpClient(),
-//         provideHttpClientTesting()
-//       ]
-//     }).compileComponents();
-//
-//     fixture = TestBed.createComponent(Trinity);
-//     component = fixture.componentInstance;
-//     await fixture.whenStable();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
