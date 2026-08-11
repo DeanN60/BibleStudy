960e0a9839f6efb4000d7189e07f3a5fe4636c69
DeanN60 <dneighbour54@gmail.com>
2026-08-11 12:35:33 -0400

    add verse

--- public/assets/data/books/Daniel/daniel_9.ts
+++ public/assets/data/books/Daniel/daniel_9.ts
@@ -1,5 +1,11 @@
 import {BookData} from '@core/book-data';
 
+export const daniel_9_26 = new BookData(
+  "Daniel 9:26 LSB",
+  "26 Then after the sixty-two weeks the Messiah will be cut off and have nothing, and the people of the prince who is to come will destroy the city and the sanctuary. And cits end will come with a flood; even to the end there will be war; desolations are decreed.",
+  "Bibles, Steadfast; Foundation, The Lockman . The Legacy Standard Bible - LSB (p. 3023). 316 Publishing, Inc.. Kindle Edition."
+);
+
 export const daniel_9_27 = new BookData(
   "Daniel 9:27 LSB",
   "27 And he will make a firm covenant with the many for one week, but in the middle of the week he will make sacrifice and grain offering cease; and on the wing of abominations will come one who makes desolate, even until a complete destruction, one that is decreed, is poured out on the one who makes desolate.”",
